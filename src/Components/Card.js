2bba2b3380a006281de8d71afb06ef82eafa21f0
jpwebdev <jjpwebdev@gmail.com>
2018-02-09 11:16:00 -0500

    now Card is stateless

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Card extends Component {
-
-    render() {
-        return (
-            <div 
-                id={this.props.id}
-                className="card" 
-                style={{ backgroundImage: `url("${this.props.image}")` }}
-                onClick={() => this.props.addCompare(this.props.id)}
-            >
-            </div>
-        );
-    }
+const Card = props => {
+    return (
+        <div 
+            id={props.id}
+            className="card" 
+            style={{ backgroundImage: `url("${props.image}")` }}
+            onClick={() => props.addCompare(props.id)}
+        >
+        </div>  
+    );
 }
-
 export default Card;
