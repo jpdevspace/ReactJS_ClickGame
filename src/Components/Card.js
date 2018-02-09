425e6438d6876b51611f1e53b547652abc5f612c
jpwebdev <jjpwebdev@gmail.com>
2018-02-09 10:34:32 -0500

    cards loading background img

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -8,8 +8,6 @@ class Card extends Component {
                 id={this.props.id}
                 className="card" 
                 style={{ backgroundImage: `url("${this.props.image}")` }}
-                // style={{ backgroundImage: `url("${props.image}")` }
-                // style={{backgroundColor: this.props.color}}
                 onClick={() => this.props.addCompare(this.props.id)}
             >
             </div>
